6fe2fc1e370771c221062311c7a1f10a82a27a17
chrakts <kaufen@cmja.de>
2019-04-18 18:53:50 +0200

    Umstellung auf timestamp

--- publishTime.py
+++ publishTime.py
@@ -2,9 +2,9 @@ import paho.mqtt.publish as publish
 import yaml
 
 
-import datetime
+import time
 
-with open('config.yaml') as f:
+with open('/home/pi/repositories/bridges/config.yaml') as f:
   dataMap = yaml.safe_load(f)
 
 
@@ -12,8 +12,9 @@ auth = {'username':dataMap["mqtt"]["user"], 'password':dataMap["mqtt"]["password
 
 
 
-t = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+#t = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+t =  int( time.time() )
 publish.single(dataMap["mqtt"]["timeAddress"],
-  payload=t,
+  payload=str(t),
   hostname=dataMap["mqtt"]["serverIP"],
   auth=auth)
