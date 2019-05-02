c5cb2c55f454903e6db3fdfdd73a2dac08f88c1f
Christof <chrak@cmja.de>
2019-05-02 22:01:49 +0200

    Zeitzone berücksichtigen

--- publishTime.py
+++ publishTime.py
@@ -3,6 +3,13 @@ import yaml
 
 
 import time
+from pytz import timezone
+import datetime
+
+tz = timezone('Europe/Berlin')
+utc_dt = datetime.datetime.utcfromtimestamp(time.time())
+utc_dt + tz.utcoffset(utc_dt)
+diff = tz.utcoffset(utc_dt).seconds
 
 with open('/home/pi/repositories/bridges/config.yaml') as f:
   dataMap = yaml.safe_load(f)
@@ -13,7 +20,7 @@ auth = {'username':dataMap["mqtt"]["user"], 'password':dataMap["mqtt"]["password
 
 
 #t = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-t =  int( time.time() )-946684800
+t =  int( time.time() )-946684800+diff
 publish.single(dataMap["mqtt"]["timeAddress"],
   payload=str(t),
   hostname=dataMap["mqtt"]["serverIP"],
