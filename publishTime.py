614bb415161d5038a7bc5caed24a47298c622a85
Christof <chrak@cmja.de>
2019-11-17 15:36:33 +0100

    Pfad flexibel gemacht

--- publishTime.py
+++ publishTime.py
@@ -11,13 +11,12 @@ utc_dt = datetime.datetime.utcfromtimestamp(time.time())
 utc_dt + tz.utcoffset(utc_dt)
 diff = tz.utcoffset(utc_dt).seconds
 
-with open('/home/pi/repositories/bridges/config.yaml') as f:
+
+path = (os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+with open(path+'/config.yaml') as f:
   dataMap = yaml.safe_load(f)
 
-
 auth = {'username':dataMap["mqtt"]["user"], 'password':dataMap["mqtt"]["password"]}
-
-
 
 #t = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 t =  int( time.time() )-946684800+diff
