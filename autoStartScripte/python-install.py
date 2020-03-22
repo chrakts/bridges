2a65379c40a176dfe48fb1539f7c3d7a439a9948
chrakts <kaufen@cmja.de>
2020-03-22 16:14:37 +0100

    Create python-install.py

---
+++ autoStartScripte/python-install.py
@@ -0,0 +1,12 @@
+# GNU nano 3.2
+# python_install.sh                                                                 
+# muss als sudo installiert werden
+
+
+apt-get install python3-pip, zip
+pip3 install paho-mqtt 
+pip3 install PyCRC
+pip3 install pyserial
+pip3 install PyYAML
+pip3 install pytz
+pip3 install setproctitle
