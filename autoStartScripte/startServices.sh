31d8b6e6e621778c475368ab2979e251c795b54f
Christof <chrak@cmja.de>
2020-04-06 10:01:16 +0200

    Fritzbox-Service hinzu

--- autoStartScripte/startServices.sh
+++ autoStartScripte/startServices.sh
@@ -1,3 +1,4 @@
 sudo systemctl start mqtt2FileBridge.service
 sudo systemctl start cmulti-bridge.service
 sudo systemctl start mqtt-bridge.service
+sudo systemctl start mqttFritzboxBridge.service
--- autoStartScripte/statusServices.sh
+++ autoStartScripte/statusServices.sh
@@ -1,3 +1,4 @@
-sudo systemctl status mqtt2FileBridge.service
-sudo systemctl status cmulti-bridge.service
-sudo systemctl status mqtt-bridge.service
+sudo systemctl status mqtt2FileBridge.service &
+sudo systemctl status cmulti-bridge.service &
+sudo systemctl status mqtt-bridge.service &
+sudo systemctl status mqttFritzboxBridge.service &
