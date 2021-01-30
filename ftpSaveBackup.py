e469b49013a5ca38ad2b80a8180f0844c96e1a06
Christof <chrak@cmja.de>
2021-01-30 14:33:39 +0100

    Erstausgabe

---
+++ ftpSaveBackup.py
@@ -0,0 +1,40 @@
+import ftplib
+import inspect,os
+import yaml
+
+
+path = (os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+with open(path+'/config.yaml') as f:
+  dataMap = yaml.safe_load(f)
+
+
+ftp = ftplib.FTP()
+ftp.connect(dataMap["getFtp"]["ip"],dataMap["getFtp"]["port"])
+ftp.login(dataMap["getFtp"]["user"], dataMap["getFtp"]["passwd"])
+
+for directory in dataMap["getFtp"]["directories"]:
+  print(directory["remotePath"])
+
+  ftp.cwd(directory["remotePath"])
+
+  localPath = directory["localPath"]
+
+  locFiles = os.listdir(localPath)
+
+  files = []
+  try:
+    files = ftp.nlst()
+  except (ftplib.error_perm, resp):
+    if str(resp) == "550 No files found":
+      print ("No files in this directory")
+    else:
+      raise
+
+  for remoteFile in files:
+    if remoteFile in locFiles:
+      print("skip: %s"%remoteFile)
+    else:
+      print("copy: %s"%remoteFile)
+      ftp.retrbinary("RETR " + remoteFile, open(localPath+'/'+remoteFile, 'wb').write)
+
+ftp.quit()
