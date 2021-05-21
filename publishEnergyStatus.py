5b9ba541d391f857c273c8f5accd5a7b98522c51
Christof <chrak@cmja.de>
2021-05-21 21:25:58 +0200

    Erstausgabe

---
+++ publishEnergyStatus.py
@@ -0,0 +1,38 @@
+import paho.mqtt.publish as publish
+import yaml
+
+import inspect,os
+import time
+from pytz import timezone
+import datetime
+import setproctitle
+setproctitle.setproctitle('py3-publishTime')
+import requests
+
+
+
+
+tz = timezone('Europe/Berlin')
+utc_dt = datetime.datetime.utcfromtimestamp(time.time())
+utc_dt + tz.utcoffset(utc_dt)
+diff = tz.utcoffset(utc_dt).seconds
+
+
+path = (os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+with open(path+'/config.yaml') as f:
+  dataMap = yaml.safe_load(f)
+
+auth = {'username':dataMap["mqtt"]["user"], 'password':dataMap["mqtt"]["password"]}
+
+dataMap["sonnenBatterie"]["serverAPI"]
+
+while(True):
+  r = requests.get("http://"+dataMap["sonnenBatterie"]["serverAPI"]+":"+str(dataMap["sonnenBatterie"]["serverPort"])+dataMap["sonnenBatterie"]["requestStatus"])
+  data = r.json()
+
+  for info in dataMap["sonnenBatterie"]["infoList"]:
+    print(info["name"]+": "+str(data[info["name"]]*info["sign"]))
+    
+    publish.single(info["address"],
+      payload=str(data[info["name"]]*info["sign"]), hostname=dataMap["mqtt"]["serverIP"], auth=auth)
+  time.sleep(10)
