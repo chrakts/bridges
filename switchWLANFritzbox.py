4a078c681850bba27dd007739120c6721a8b47e0
CHristof <kaufen@cmja.de>
2022-07-12 21:35:06 +0200

    user für Authentifizierung ergänzt

--- switchWLANFritzbox.py
+++ switchWLANFritzbox.py
@@ -1,14 +1,12 @@
-import inspect, os
-import signal
-from PyCRC.CRCCCITT import CRCCCITT
+import inspect
+import os
 from fritzconnection import FritzConnection
 import yaml
-import datetime
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
-import time
 import setproctitle
 setproctitle.setproctitle('py3-switchWLANFritzbox')
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -20,35 +18,35 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(info["setTopic"])
     print("Connected to "+info["setTopic"])
 
+
 def on_message(client, userdata, msg):
-  auth = {'username':dataMap["mqtt"]["user"], 'password':dataMap["mqtt"]["password"]}
+  auth = {'username': dataMap["mqtt"]["user"], 'password': dataMap["mqtt"]["password"]}
   topic = str(msg.topic)
-  payload= (msg.payload).decode("utf-8")
+  payload = msg.payload.decode("utf-8")
   state = int(payload)
-  print((topic)+" : "+(payload))
+  print(topic + " : " + payload)
   for info in dataMap["fritzBoxMain"]["switches"]:
-    if info["setTopic"]==topic:
+    if info["setTopic"] == topic:
       command = info["command"]
       print(command+": "+str(state))
-      fc = FritzConnection(dataMap["fritzBoxMain"]["ip"],password=dataMap["fritzBoxMain"]["password"])
+      fc = FritzConnection(dataMap["fritzBoxMain"]["ip"], user=dataMap["fritzBoxMain"]["user"], password=dataMap["fritzBoxMain"]["password"])
       fc.call_action(command, 'SetEnable', NewEnable=state)
-      state = str(  int( fc.call_action(command, 'GetInfo')["NewEnable"] ) )
+      state = str(int(fc.call_action(command, 'GetInfo')["NewEnable"]))
       print("State: "+state)
       try:
         publish.single(info["getTopic"], payload=state, hostname=dataMap["mqtt"]["serverIP"], auth=auth)
       except Exception as e:
-        print('Failed: '+ str(e))
+        print('Failed: ' + str(e))
       print("published")
-  
+
+
 path = (os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 with open(path+'/config.yaml') as f:
   dataMap = yaml.safe_load(f)
   
-client = mqtt.Client( )
+client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.username_pw_set(username=dataMap["mqtt"]["user"],password=dataMap["mqtt"]["password"])
+client.username_pw_set(username=dataMap["mqtt"]["user"], password=dataMap["mqtt"]["password"])
 client.connect(dataMap["mqtt"]["serverIP"], port=int(dataMap["mqtt"]["port"]),  keepalive=60)
 client.loop_forever()
-
-
