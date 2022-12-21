7d0168a21cd536e8d032c5fa783655679d5f30ac
chrak <kaufen@cmja.de>
2022-12-21 20:52:40 +0100

    Erstausgabe

---
+++ publishFreeDiskSpace.py
@@ -0,0 +1,19 @@
+import psutil
+import paho.mqtt.publish as publish
+import yaml
+import inspect,os
+import setproctitle
+setproctitle.setproctitle('py3-publishTime')
+
+hdd = psutil.disk_usage('/')
+
+path = (os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+with open(path+'/config.yaml') as f:
+  dataMap = yaml.safe_load(f)
+
+auth = {'username': dataMap["mqtt"]["user"], 'password': dataMap["mqtt"]["password"]}
+
+publish.single(dataMap["mqtt"]["diskFreeAddress"],
+  payload=str(hdd.free//(2**32)),
+  hostname=dataMap["mqtt"]["serverIP"],
+  auth=auth)
