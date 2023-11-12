0348ea309db3e0825e7b34a9860f7ee5024aa649
chrak <kaufen@cmja.de>
2023-11-12 21:05:16 +0100

    .

--- HeizungAuswertungen/HeizungJahresMittel.py
+++ HeizungAuswertungen/HeizungJahresMittel.py
@@ -13,7 +13,7 @@ dataPath = dataMap["mqtt2FileBridge"]["dataFolder"]
 targetPath = dataMap["auswertungen"]["dataFolder"]
 
 resultData = pd.read_csv(targetPath+'/HeizungVerbrauchSummary.csv',  sep=';', parse_dates={'Date': [1]})
-resultData = resultData.rolling(7).mean()
-resultData.reset_index(level=0, inplace=True)
-meanYear = resultData.groupby([resultData['Datum'].dt.month, resultData['Datum'].dt.day]).mean()
-meanYear.to_csv(targetPath+'/meanYear.csv', sep=';')
+resultData['Heizleistung [l]'] = resultData['Heizleistung [l]'].rolling(7).mean()
+meanYear = resultData.groupby([resultData['Date'].dt.month, resultData['Date'].dt.day]).mean()
+meanYear.to_csv(targetPath+'/HeizungJahresMittel.csv',  sep=';', columns=['Heizleistung [l]'])
+print(meanYear)
