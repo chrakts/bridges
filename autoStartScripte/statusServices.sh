883627cb9e73a726b92a2ddf41d960c282a6ab02
christof <chrak@cmja.de>
2025-04-16 18:35:05 +0200

    Anpassung Python

--- autoStartScripte/statusServices.sh
+++ autoStartScripte/statusServices.sh
@@ -2,5 +2,5 @@ sudo systemctl status mqtt2FileBridge.service &
 sudo systemctl status cmulti-bridge.service &
 sudo systemctl status mqtt-bridge.service &
 sudo systemctl status mqttFritzboxBridge.service &
-sudo systemctl status publishEnergyStatus.service
-sudo systemctl status publishSonnenStatus.service
+sudo systemctl status publishEnergyStatus.service &
+sudo systemctl status publishSonnenStatus.service 
